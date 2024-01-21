c828fd3bed9be357aea86b75122dc7807361f414
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-01-21 11:58:49 +0330

    add search price

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 const searchInput=document.getElementById("search-input");
 const products=document.querySelectorAll(".product-item");
 const buttons=document.querySelectorAll(".filter");
+const priceButton=document.getElementById("search-price").querySelector("button");
+
+const changeClass= (filter) => {
+    buttons.forEach(button => {
+        if(button.dataset.filter===filter){
+            button.classList.add("selected");
+        }else{
+            button.classList.remove("selected");
+        }
+    })
+}
 const searchHandler= (event) => {
 const searchValue=event.target.value.toLowerCase().trim();
 
@@ -16,10 +27,33 @@ products.forEach(product => {
 
 const filterHandler= (event) => {
 const filter=event.target.dataset.filter;
-console.log(filter);
+changeClass(filter);
+products.forEach((product) => {
+    const category=product.dataset.category;
+    
+    if(filter==="All"){
+         product.style.display="block";
+    }else{
+     filter===category ? 
+     (product.style.display="block") :
+      (product.style.display="none");
+    }
+})
 }
 
-searchInput.addEventListener("keyup",searchHandler);
+const searchPriceHandler= (event) => {
+    const searchPrice=+event.target.parentElement.children[0].value;
+    console.log(searchPrice);
+
+    products.forEach(product => {
+        const productPrice=product.children[2].innerText;
+        const price=productPrice.split(" ")[1];
+        console.log(price);
+    })
+}
+
 buttons.forEach(button => {
     button.addEventListener("click",filterHandler)
 })
+searchInput.addEventListener("keyup",searchHandler);
+priceButton.addEventListener("click",searchPriceHandler);
